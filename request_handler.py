60de3f14f9a62b0706a19557726b63c4043c9c53
Billy Campbell <wcampbell91@gmail.com>
2020-10-06 18:56:39 -0500

    setup

---
+++ request_handler.py
@@ -0,0 +1,66 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from animals import get_all_animals
+
+
+# Here's a class. It inherits from another class.
+class HandleRequests(BaseHTTPRequestHandler):
+
+    # Here's a class function
+    def _set_headers(self, status):
+        self.send_response(status)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    # Here's a method on the class that overrides the parent's method.
+    # It handles any GET request.
+    def do_GET(self):
+        # Set the response code to 'Ok'
+        self._set_headers(200)
+
+        # Your new console.log() that outputs to the terminal
+        print(self.path)
+
+        # It's an if..else statement
+        if self.path == "/animals":
+            response = get_all_animals()
+        elif self.path == "/locations":
+            response = [
+                { "id": 3, "name": "Zoo" },
+                { "id": 4, "name": "Kennel" }
+            ]
+        elif self.path == "/employees":
+            response = [
+                { "id": 5, "name": "Billy" },
+                { "id": 6, "name": "Taylor" }
+            ]
+        else:
+            response = []
+
+        # This weird code sends a response back to the client
+        self.wfile.write(f"{response}".encode())
+
+    # Here's a method on the class that overrides the parent's method.
+    # It handles any POST request.
+    def do_POST(self):
+        # Set response code to 'Created'
+        self._set_headers(201)
+
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        response = f"received post request:<br>{post_body}"
+        self.wfile.write(response.encode())
+
+
+    # Here's a method on the class that overrides the parent's method.
+    # It handles any PUT request.
+    def do_PUT(self):
+        self.do_POST()
+
+
+# This function is not inside the class. It is the starting
+# point of this application.
+def main():
+    host = ''
+    port = 8088
+    HTTPServer((host, port), HandleRequests).serve_forever()
