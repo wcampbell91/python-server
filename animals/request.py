e27c79213e56682b9f9bef9bd6655d0132715e70
Billy Campbell <wcampbell91@gmail.com>
2020-10-13 21:40:05 -0500

    get animals by status

--- animals/request.py
+++ animals/request.py
@@ -147,3 +147,29 @@ def get_animal_by_location(location_id):
             animals.append(animal.__dict__)
         
     return json.dumps(animals)
+
+def get_animals_by_status(status):
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.breed,
+            a.status,
+            a.customer_id,
+            a.location_id
+        FROM animal a
+        WHERE a.status = ?
+        """, (status, ))
+
+        animals = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            animal = Animal(row['id'], row['name'], row['breed'], row['status'], row['location_id'], row['customer_id'])
+            animals.append(animal.__dict__)
+
+    return json.dumps(animals)
--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal, get_animal_by_location
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal, get_animal_by_location, get_animals_by_status
 from employees import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee, get_employee_by_location
 from locations import get_all_locations, get_single_location, create_location, delete_location, update_location
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer, get_customers_by_email
@@ -59,6 +59,8 @@ class HandleRequests(BaseHTTPRequestHandler):
                 response = get_animal_by_location(value)
             elif key == "location_id" and resource == "employees":
                 response = get_employee_by_location(value)
+            elif key == "status" and resource == "animals":
+                response = get_animals_by_status(value)
         
         self.wfile.write(response.encode())
 
